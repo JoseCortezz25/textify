33f20dc71fc66e9806af9547807d727bfcea0fd4
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2024-04-21 18:34:17 -0500

    Update page.tsx to fix SelectTrigger onChange event and setTone function

--- src/app/copy/page.tsx
+++ src/app/copy/page.tsx
@@ -134,12 +134,11 @@ const PageCopy = () => {
           <Label className="subtitle-group" htmlFor="message">Elige el tono</Label>
 
           <Select>
-            <SelectTrigger className="w-full" onChange={(e) => setTone('profesional')}>
+            <SelectTrigger className="w-full" onChange={(value: TONES) => setTone(value)}>
               <SelectValue placeholder="Elige el tono de tu preferencia" />
             </SelectTrigger>
             <SelectContent>
-              <SelectItem value="profesional" className="cursor-pointer
-">
+              <SelectItem value={TONES.PROFESSIONAL} className="cursor-pointer">
                 <div className="flex space-x-3 items-center py-2">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
@@ -148,8 +147,7 @@ const PageCopy = () => {
                   <p>Profesional</p>
                 </div>
               </SelectItem>
-              <SelectItem value="antusiasmado" className="cursor-pointer
-">
+              <SelectItem value={TONES.ENTHUSIASTIC} className="cursor-pointer">
                 <div className="flex space-x-3 items-center py-2">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
@@ -158,8 +156,7 @@ const PageCopy = () => {
                   <p>Entusiasta</p>
                 </div>
               </SelectItem>
-              <SelectItem value="informativo" className="cursor-pointer
-">
+              <SelectItem value={TONES.INFORMATIVE} className="cursor-pointer">
                 <div className="flex space-x-3 items-center py-2">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
@@ -168,8 +165,7 @@ const PageCopy = () => {
                   <p>Informativo</p>
                 </div>
               </SelectItem>
-              <SelectItem value="divertido" className="cursor-pointer
-">
+              <SelectItem value={TONES.FUNNY} className="cursor-pointer">
                 <div className="flex space-x-3 items-center py-2">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z" />
@@ -178,8 +174,7 @@ const PageCopy = () => {
                   <p>Divertido</p>
                 </div>
               </SelectItem>
-              <SelectItem value="informal" className="cursor-pointer
-">
+              <SelectItem value={TONES.INFORMAL} className="cursor-pointer">
                 <div className="flex space-x-3 items-center py-2">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
@@ -218,7 +213,6 @@ const PageCopy = () => {
                   <p>Bullet</p>
                 </div>
               </SelectItem>
-
             </SelectContent>
           </Select>
         </div>
